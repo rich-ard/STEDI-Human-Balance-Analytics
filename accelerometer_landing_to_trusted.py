69d8b63382acb914fac3a9d3c2ca8f4dac13e391
Richard Brandt <rich-ard@users.noreply.github.com>
2025-01-19 16:01:31 -0500

    rewritten in glue visual ETL

--- accelerometer_landing_to_trusted.py
+++ accelerometer_landing_to_trusted.py
@@ -26,59 +26,28 @@ DEFAULT_DATA_QUALITY_RULESET = """
     ]
 """
 
-# Script generated for node Accelerometer Landing
-AccelerometerLanding_node1 = glueContext.create_dynamic_frame.from_options(
-    format_options={"multiline": False},
-    connection_type="s3",
-    format="json",
-    connection_options={
-        "paths": ["s3://frequently-modulated/accelerometer/landing/"],
-        "recurse": True,
-    },
-    transformation_ctx="AccelerometerLanding_node1",
-)
+# Script generated for node Accelerometer Landing Zone
+AccelerometerLandingZone_node1737319792305 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="accelerometer_landing", transformation_ctx="AccelerometerLandingZone_node1737319792305")
 
 # Script generated for node Customer Trusted
-CustomerTrusted_node1 = glueContext.create_dynamic_frame.from_options(
-    format_options={"multiline": False},
-    connection_type="s3",
-    format="json",
-    connection_options={"paths": ["s3://frequently-modulated/customer/trusted/"], "recurse": True},
-    transformation_ctx="CustomerTrusted_node1",
-)
+CustomerTrusted_node1737319819749 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="customer_landing", transformation_ctx="CustomerTrusted_node1737319819749")
 
-# Script generated for node Customer Privacy Filter
-CustomerPrivacyFilter_node1 = Join.apply(
-    frame1=AccelerometerLanding_node1,
-    frame2=CustomerTrusted_node1,
-    keys1=["user"],
-    keys2=["email"],
-    transformation_ctx="CustomerPrivacyFilter_node1",
-)
+# Script generated for node Join Customer
+JoinCustomer_node1737319842571 = Join.apply(frame1=CustomerTrusted_node1737319819749, frame2=AccelerometerLandingZone_node1737319792305, keys1=["email"], keys2=["user"], transformation_ctx="JoinCustomer_node1737319842571")
 
-# Script generated for node Drop Fields
-DropFields_node1 = ApplyMapping.apply(
-    frame=CustomerPrivacyFilter_node1,
-    mappings=[
-        ("user", "string", "user", "string"),
-        ("timeStamp", "long", "timeStamp", "long"),
-        ("x", "double", "x", "float"),
-        ("y", "double", "y", "float"),
-        ("z", "double", "z", "float"),
-    ],
-    transformation_ctx="DropFields_node1",
-)
+# Script generated for node SQL Query
+SqlQuery0 = '''
+select
+x , y, z, user, timestamp
+from myDataSource
+
+'''
+SQLQuery_node1737320117642 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"myDataSource":JoinCustomer_node1737319842571}, transformation_ctx = "SQLQuery_node1737320117642")
 
 # Script generated for node Accelerometer Trusted
-AccelerometerTrusted_node1 = glueContext.write_dynamic_frame.from_options(
-    frame=DropFields_node1,
-    connection_type="s3",
-    format="json",
-    connection_options={
-        "path": "s3://frequently-modulated/accelerometer/trusted/",
-        "partitionKeys": [],
-    },
-    transformation_ctx="AccelerometerTrusted_node1",
-)
-
+EvaluateDataQuality().process_rows(frame=SQLQuery_node1737320117642, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1737319172847", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
+AccelerometerTrusted_node1737319853221 = glueContext.getSink(path="s3://frequently-modulated/accelerometer/trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], compression="snappy", enableUpdateCatalog=True, transformation_ctx="AccelerometerTrusted_node1737319853221")
+AccelerometerTrusted_node1737319853221.setCatalogInfo(catalogDatabase="stedi",catalogTableName="accelerometer_trusted")
+AccelerometerTrusted_node1737319853221.setFormat("json")
+AccelerometerTrusted_node1737319853221.writeFrame(SQLQuery_node1737320117642)
 job.commit()
