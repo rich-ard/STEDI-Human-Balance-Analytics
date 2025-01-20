4a589d01e1b39597f11f9986e1302eeda4441d31
Richard Brandt <rich-ard@users.noreply.github.com>
2025-01-20 12:05:32 -0500

    Update accelerometer_landing.sql

--- SQL_DDL_scripts/accelerometer_landing.sql
+++ SQL_DDL_scripts/accelerometer_landing.sql
@@ -1,12 +1,16 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS `stedi`.`accelerometer_landing` (
-  `user` string
-  , `timeStamp` bigint
-  , `x` float
-  , `y` float
-  , `z` float
-  )
-ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
-WITH SERDEPROPERTIES (
-  'serialization.format' = `
-) LOCATION 's3://frequently-modulated-lakehouse/accelerometer/landing'
-TBLPROPERTIES ('has_encrypted_data'='false');
+CREATE EXTERNAL TABLE `accelerometer_landing`(
+  `user` string COMMENT 'from deserializer', 
+  `timestamp` bigint COMMENT 'from deserializer', 
+  `x` float COMMENT 'from deserializer', 
+  `y` float COMMENT 'from deserializer', 
+  `z` float COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://frequently-modulated/accelerometer/landing/'
+TBLPROPERTIES (
+  'classification'='json')
